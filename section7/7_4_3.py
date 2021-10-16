c6acc4e056bd261d59a9fc927ed37ea44d17e0da
taka <popporz33@gmail.com>
2021-10-16 18:46:25 +0900

    add section7

---
+++ section7/7_4_3.py
@@ -0,0 +1,62 @@
+import numpy
+
+
+def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
+    """
+    Parameters
+    ----------
+    input_data : (データ数, チャンネル, 高さ, 幅)の4次元配列からなる入力データ
+    filter_h : フィルターの高さ
+    filter_w : フィルターの幅
+    stride : ストライド
+    pad : パディング
+    Returns
+    -------
+    col : 2次元配列
+    """
+    N, C, H, W = input_data.shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+
+    img = numpy.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
+    col = numpy.zeros((N, C, filter_h, filter_w, out_h, out_w))
+
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
+
+    col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
+    return col
+
+
+class Convolution:
+    def __init__(self, W, b, stride=1, pad=0):
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2*self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2*self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, self.stride, self.pad)
+        col_W = self.W.reshape(FN, -1).T
+        out = numpy.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
+
+
+x1 = numpy.random.rand(1, 3, 7, 7)
+col1 = im2col(x1, 5, 5, stride=1, pad=0)
+print(col1.shape)
+
+x2 = numpy.random.rand(10, 3, 7, 7)
+col2 = im2col(x2, 5, 5, stride=1, pad=0)
+print(col2.shape)
